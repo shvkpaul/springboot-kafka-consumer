ba20a06099a6c97e6e8c43445eccc27efaf6c2e2
Shouvik Paul <shouvik.paul@ah.nl>
2023-07-01 21:31:01 +0200

    added manual offset demo

--- src/main/java/com/shvk/libraryeventconsumer/config/LibraryEventConsumerConfig.java
+++ src/main/java/com/shvk/libraryeventconsumer/config/LibraryEventConsumerConfig.java
@@ -1,8 +1,36 @@
 package com.shvk.libraryeventconsumer.config;
 
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.ContainerCustomizer;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 
 @Configuration
 //@EnableKafka
 public class LibraryEventConsumerConfig {
+    private final KafkaProperties properties;
+
+    public LibraryEventConsumerConfig(KafkaProperties properties) {
+        this.properties = properties;
+    }
+
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(
+        ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
+        ObjectProvider<ConsumerFactory<Object, Object>> kafkaConsumerFactory,
+        ObjectProvider<ContainerCustomizer<Object, Object, ConcurrentMessageListenerContainer<Object, Object>>> kafkaContainerCustomizer) {
+        ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        configurer.configure(factory, kafkaConsumerFactory
+            .getIfAvailable(() -> new DefaultKafkaConsumerFactory<>(this.properties.buildConsumerProperties())));
+        factory.setConcurrency(3);
+        //factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+        // we will be using default batch commit
+        return factory;
+    }
 }
---
+++ src/main/java/com/shvk/libraryeventconsumer/consumer/LibraryEventsConsumerManualOffset.java
@@ -0,0 +1,19 @@
+package com.shvk.libraryeventconsumer.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.listener.AcknowledgingMessageListener;
+import org.springframework.kafka.support.Acknowledgment;
+
+//@Component
+@Slf4j
+public class LibraryEventsConsumerManualOffset implements AcknowledgingMessageListener<Integer, String> {
+    //This is not being used just for a showcase
+    @Override
+    @KafkaListener(topics = {"library-events"})
+    public void onMessage(ConsumerRecord<Integer, String> consumerRecord, Acknowledgment acknowledgment) {
+        log.info("ConsumerRecord in Manual Offset Consumer: {} ", consumerRecord);
+        acknowledgment.acknowledge();
+    }
+}
